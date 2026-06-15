85bc8650a0637490b3d2bd6cff9325c666e4f58b
fromscode <chakrabortyshubham00@gmail.com>
2026-06-15 12:10:23 +0530

    Adding priority to test cases

--- src/test/java/org/example/FirstTestCase.java
+++ src/test/java/org/example/FirstTestCase.java
@@ -3,17 +3,17 @@ package org.example;
 import org.testng.annotations.Test;
 
 public class FirstTestCase {
-    @Test
+    @Test(priority = 1)
     void openApp() {
         System.out.println("Opening App");
     }
 
-    @Test
+    @Test(priority = 2)
     void login() {
         System.out.println("Logging in");
     }
 
-    @Test
+    @Test(priority = 3)
     void logout() {
         System.out.println("Logging out");
     }
